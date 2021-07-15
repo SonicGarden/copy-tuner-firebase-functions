30271b0572d467b3d064dfe4bd086c11c490f8aa
hiroro-work <hiroro@sonicgarden.jp>
2021-07-15 17:46:02 +0900

    stagingの場合のみurl返すように

--- src/getUrl.ts
+++ src/getUrl.ts
@@ -2,10 +2,10 @@ import * as functions from 'firebase-functions';
 
 export const getUrl = async (): Promise<{ url: string }> => {
   const {
-    copy_tuner: { host, api_key: apiKey },
+    copy_tuner: { host, api_key: apiKey, environment },
   } = functions.config();
 
-  return { url: `${host}/projects/${apiKey}` };
+  return { url: environment === 'staging' ? `${host}/projects/${apiKey}` : '' };
 };
 
 export const getCopyTunerUrl = functions.region('asia-northeast1').https.onCall(async () => {
