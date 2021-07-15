65856acd21db4d1965a776d2d1151779aa2c17e2
hiroro-work <hiroro@sonicgarden.jp>
2021-07-15 15:51:10 +0900

    reafactor

--- src/getUrl.ts
+++ src/getUrl.ts
@@ -1,9 +1,11 @@
 import * as functions from 'firebase-functions';
 
 export const getUrl = async (): Promise<string> => {
-  const { copy_tuner: copyTuner } = functions.config();
+  const {
+    copy_tuner: { host, api_key: apiKey },
+  } = functions.config();
 
-  return `${copyTuner.host}/projects/${copyTuner.api_key}`;
+  return `${host}/projects/${apiKey}`;
 };
 
 export const getCopyTunerUrl = functions.region('asia-northeast1').https.onCall(async () => {
