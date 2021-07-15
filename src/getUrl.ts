77563ac3f71fa82f4643907921b2ba17c4abb480
hiroro-work <hiroro@sonicgarden.jp>
2021-07-15 15:58:43 +0900

    fix a bit

--- src/getUrl.ts
+++ src/getUrl.ts
@@ -1,11 +1,11 @@
 import * as functions from 'firebase-functions';
 
-export const getUrl = async (): Promise<string> => {
+export const getUrl = async (): Promise<{ url: string }> => {
   const {
     copy_tuner: { host, api_key: apiKey },
   } = functions.config();
 
-  return `${host}/projects/${apiKey}`;
+  return { url: `${host}/projects/${apiKey}` };
 };
 
 export const getCopyTunerUrl = functions.region('asia-northeast1').https.onCall(async () => {
